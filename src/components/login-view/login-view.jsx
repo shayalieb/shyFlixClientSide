3972a0d9d4d4c578deb11b9efd00b0b45c6133f3
shayalieb <105383935+shayalieb@users.noreply.github.com>
2023-02-14 13:51:54 -0500

    Update login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -41,7 +41,6 @@ export const LoginView = () => {
                     console.log(data)
                     dispatch(setUser(data.user));
                     dispatch(setToken(data.token))
-                    window.location.reload();
                 } else {
                     alert('The user does not exist');
                 }
